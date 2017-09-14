b35adb6836a8c12f9c792e12bceb9eba9325c9e8
Joshua Abe <joshua.t.abe@gmail.com>
2017-09-14 08:43:11 -0700

    Defined add function. Finished lab 1.

---
+++ Labs/Lab1/Add.cpp
@@ -0,0 +1,7 @@
+#include "Add.h"
+
+// Takes two integers and returns the sum
+int lab1::Add(int a, int b)
+{
+	return a + b;
+}
---
+++ Labs/Lab1/main.cpp
@@ -0,0 +1,13 @@
+#include "Add.h"
+
+#include <iostream>
+
+using namespace lab1;
+using namespace std;
+
+int main()
+{
+	cout << Add(1, 2) << endl;
+
+	return 0;
+}
