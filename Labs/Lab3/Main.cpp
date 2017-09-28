577fd71713f484499a592f92b922dcb1076064a9
Joshua Abe <joshua.t.abe@gmail.com>
2017-09-28 09:46:46 -0700

    Build 2

---
+++ Labs/Lab3/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "TimeSheet.h"
+
+using namespace lab3;
+
+int main()
+{
+	TimeSheet* sheet = new TimeSheet("Bob", 3);
+	sheet->AddTime(3);
+	TimeSheet* copy = new TimeSheet(*sheet);
+	sheet->AddTime(1);
+
+	std::cout << sheet->GetTotalTime() << std::endl;
+	std::cout << copy->GetTotalTime() << std::endl;
+	system("pause");
+	return 0;
+};
