c94d5ff58e47d0da5e0da053a9e9dc91b25dd856
Joshua Abe <joshua.t.abe@gmail.com>
2017-12-06 22:56:13 -0800

    First build

---
+++ Labs/Lab8/Storage.h
@@ -0,0 +1,72 @@
+#pragma once
+
+namespace lab8
+{
+	template <typename T>
+	class Storage
+	{
+	public:
+		Storage(int length);
+		Storage(const Storage& other);
+		Storage(Storage&& other);
+
+		Storage& operator=(Storage&& other);
+
+		const T* GetData() const;
+		int GetLength() const;
+		virtual ~Storage();
+
+	private:
+		T* mData;
+		int mLength;
+	};
+
+	template <typename T>
+	Storage<T>::Storage(int length)
+	{
+		mData = new T[length];
+		mLength = length;
+	}
+
+	template <typename T>
+	Storage<T>::Storage(const Storage& other)
+		: mLength(other.mLength)
+	{
+		mData = new T[other.mLength];
+		memcpy(mData, other.mData, other.mLength);
+	}
+
+	template <typename T>
+	Storage<T>::Storage(Storage&& other)
+		: mData(other.mData)
+		, mLength(other.mLength)
+	{
+		other.mData = nullptr;
+		other.mLength = 0;
+	}
+
+	template <typename T>
+	Storage<T>& Storage<T>::operator=(Storage&& other)
+	{
+		Storage output(std::move(other));
+		return output;
+	}
+
+	template <typename T>
+	const T* Storage<T>::GetData() const
+	{
+		return mData;
+	}
+
+	template <typename T>
+	int Storage<T>::GetLength() const
+	{
+		return mLength;
+	}
+
+	template <typename T>
+	Storage<T>::~Storage()
+	{
+		delete mData;
+	}
+}
---
+++ Labs/Lab8/main.cpp
@@ -0,0 +1,20 @@
+#include "Storage.h"
+#include <utility>
+
+int main()
+{
+	// using move constructor
+	lab8::Storage<int> storage1(10000);
+	lab8::Storage<int> storage1Copy(std::move(storage1)); // storage1's memory should be nulled/zeroed
+
+	// using assignment operator
+	lab8::Storage<char> storage2(20000);
+	lab8::Storage<char> storage2Copy(3);
+	storage2Copy = std::move(storage2); // storage2's memory should be nulled/zeroed
+
+	// using regular copy constructor
+	lab8::Storage<double> storage3(10000000);
+	lab8::Storage<double> storage3Copy(storage3); // storage3 still has it's own data
+
+	return 0;
+}
